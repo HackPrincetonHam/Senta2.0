1319c89d2feee5002fb85ef774a5d0f25ac2233a
PeterWiIIiam <32248504+PeterWiIIiam@users.noreply.github.com>
2017-10-31 21:21:56 -0400

    comment in every  line

---
+++ CognitoYourUserPoolsSample/Constants.swift
@@ -0,0 +1,26 @@
+//
+// Copyright 2014-2017 Amazon.com,
+// Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Amazon Software License (the "License").
+// You may not use this file except in compliance with the
+// License. A copy of the License is located at
+//
+//     http://aws.amazon.com/asl/
+//
+// or in the "license" file accompanying this file. This file is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, express or implied. See the License
+// for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+import AWSCognitoIdentityProvider
+
+let CognitoIdentityUserPoolRegion: AWSRegionType = AWSRegionType.USEast1
+let CognitoIdentityUserPoolId = "us-east-1_2nqNLwani"
+let CognitoIdentityUserPoolAppClientId = "4e9o01igfgqour9eh7gdssvqr3"
+let CognitoIdentityUserPoolAppClientSecret = "1ft1ab36bkok684n8i9frfvvdklf58vqp6ltukbou72l17nfi93h"
+
+let AWSCognitoUserPoolsSignInProviderKey = "UserPool"
